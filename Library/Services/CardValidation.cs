006170910a94affd3ecd1d96a3729adb74ed4af7
roo-k13 <95150355+roo-k13@users.noreply.github.com>
2023-02-06 15:07:00 +0000

    Created a new class for card validation

---
+++ Library/Services/CardValidation.cs
@@ -0,0 +1,20 @@
+﻿using Library.Data;
+using Library.Models;
+
+namespace Library.Services
+{
+    public class CardValidation
+    {
+        public Account? AuthenticatedAccount { get; init; }
+
+        public CardValidation(string number, string pin)
+        {
+            Card? card = Database.FindCardByNumber(number);
+
+            if (card == null || Database.IsPinCorrect(card, pin) == false)
+                return;
+
+            AuthenticatedAccount = Database.FindAccountByCardNumber(number);
+        }
+    }
+}
